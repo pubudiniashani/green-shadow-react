33beb4bbe6333ebac4f9b11ecaea33f9baad2d35
pubudiniashani <pubudiniashani2000@gmail.com>
2025-01-20 13:12:22 +0530

    login update

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -8,13 +8,13 @@ export function LoginForm() {
                     <label htmlFor="email" className="form-label">Email</label>
                     {/*<input type="text" className="form-control" id="email"/>*/}
                     <input
-                                                type="email"
-                                                name="email"
-                                                id="email"
-                                                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                placeholder="example@gmail.com"
-                                                required
-                                            />
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="example@gmail.com"
+                        required
+                    />
 
                 </div>
                 <div className="mb-3">
@@ -26,11 +26,25 @@ export function LoginForm() {
                         id="password"
                         placeholder="••••••••"
                         className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                required
-                                            />
+                        required
+                    />
                 </div>
 
-                <button className="btn btn-primary btnlogin" id="login-btn">LOGIN</button>
+                {/*<button*/}
+                {/*    className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"*/}
+                {/*    id="login-btn"*/}
+                {/*>*/}
+                {/*    LOGIN*/}
+                {/*</button>*/}
+
+                <div className="flex justify-center mt-8">
+                    <button
+                        className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+                        id="login-btn"
+                    >
+                        LOGIN
+                    </button>
+                </div>
 
             </div>
         </div>
