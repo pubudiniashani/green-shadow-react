721ccc20e24db12c31f0f63f50e3ee9118ab4928
pubudiniashani <pubudiniashani2000@gmail.com>
2025-02-26 18:45:45 +0530

    button changed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
-export function Button(props:{text:string,onClick: () => void }){
+export function Button(props:{text:string,onClick: () => void ,styles:string}){
     return (
         <button
             onClick={props.onClick}
-            className="bg-blue-400 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
-            id="signup-btn"
+            className= {props.styles}
+            id="btn"
         >
             {props.text}
         </button>
